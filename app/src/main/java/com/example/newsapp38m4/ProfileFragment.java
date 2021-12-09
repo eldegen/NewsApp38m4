218e5bc7457866b03e1a34c404700b09d6dfe4ef
eldegen <eliyaz2761@gmail.com>
2021-12-10 03:22:56 +0600

    Сделал логику с аватаркой в профиле

--- app/src/main/java/com/example/newsapp38m4/ProfileFragment.java
+++ app/src/main/java/com/example/newsapp38m4/ProfileFragment.java
@@ -1,6 +1,10 @@
 package com.example.newsapp38m4;
 
+import static android.app.Activity.RESULT_OK;
+
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -9,14 +13,20 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.newsapp38m4.databinding.FragmentProfileBinding;
 
+import java.io.IOException;
+
 public class ProfileFragment extends Fragment {
     private FragmentProfileBinding binding;
+    private static Uri avatarBuffer;
+    static final int REQUEST_CODE = 1;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -28,11 +38,36 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        binding.ivAvatar.setImageURI(avatarBuffer);
+
         binding.ivAvatar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent();
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, REQUEST_CODE);
             }
         });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+
+        Bitmap bitmap = null;
+
+        switch(requestCode) {
+            case REQUEST_CODE:
+                if(resultCode == RESULT_OK){
+                    Uri selectedImage = imageReturnedIntent.getData();
+                    avatarBuffer = selectedImage;
+                    try {
+                        bitmap = MediaStore.Images.Media.getBitmap(getActivity().getApplicationContext().getContentResolver(), selectedImage);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    binding.ivAvatar.setImageBitmap(bitmap);
+                }
+        }
     }
 }
