0dab4b04771860dfd8d68fc37b97f8ccbb32a8db
eldegen <eliyaz2761@gmail.com>
2021-12-10 01:31:47 +0600

    Initial commit

---
+++ app/src/main/java/com/example/newsapp38m4/NewFragment.java
@@ -0,0 +1,53 @@
+package com.example.newsapp38m4;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.newsapp38m4.databinding.FragmentNewBinding;
+
+public class NewFragment extends Fragment {
+    private FragmentNewBinding binding;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentNewBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                open();
+            }
+        });
+    }
+
+    private void open() {
+        String text = binding.editText.getText().toString();
+        Bundle bundle = new Bundle();
+        bundle.putString("new.text", text);
+        getParentFragmentManager().setFragmentResult("rk.news", bundle);
+        Log.e("f_home", "reached");
+        close();
+    }
+
+    private void close() {
+        NavController navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment_activity_main);
+        navController.navigateUp();
+    }
+}
