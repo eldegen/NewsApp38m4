5d3a210020ef7c9a20279d89f6e5b83e0d05dff4
eldegen <eliyaz2761@gmail.com>
2021-12-17 00:53:11 +0600

    I finished my BoardAdapter

--- app/src/main/java/com/example/newsapp38m4/ui/board/BoardAdapter.java
+++ app/src/main/java/com/example/newsapp38m4/ui/board/BoardAdapter.java
@@ -3,6 +3,9 @@ package com.example.newsapp38m4.ui.board;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -10,6 +13,9 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.newsapp38m4.R;
 
 public class BoardAdapter extends RecyclerView.Adapter<BoardAdapter.ViewHolder> {
+    private String[] titles = new String[] {"a", "b", "c"};
+    private String[] subtitles = new String[] {"sub a", "sub b", "sub c"};
+    private int[] images = new int[] {R.drawable.ic_newspage, R.drawable.img_newspaper, R.drawable.img_news_globus};
 
     @NonNull
     @Override
@@ -19,17 +25,29 @@ public class BoardAdapter extends RecyclerView.Adapter<BoardAdapter.ViewHolder> 
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+        holder.onBind(position);
     }
 
     @Override
     public int getItemCount() {
-        return 3;
+        return titles.length;
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView tvTitle, tvSubtitle;
+        private ImageView imageView;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            tvTitle = itemView.findViewById(R.id.tv_title);
+            tvSubtitle = itemView.findViewById(R.id.tv_subtitle);
+            imageView = itemView.findViewById(R.id.image_view);
+        }
+
+        public void onBind(int position) {
+            tvTitle.setText(titles[position]);
+            tvSubtitle.setText(subtitles[position]);
+            imageView.setImageResource(images[position]);
         }
     }
 }
